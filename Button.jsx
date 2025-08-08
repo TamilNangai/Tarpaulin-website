db818e616a1a2a351f1b7507f7ffd972e136a120
Atchayasri1022 <atchayasribalasundaram@gmail.com>
2025-08-08 20:22:02 +0530

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,8 @@
+import btn from'./assets/view/Next.png'
+
+function Button() {
+  return(
+    <button className="flex items-center px-2 border border-black py-2 w-30 m-5 font-semibold bg-blue-500 text-black rounded-full">View More<img className="ml-2" src={btn} alt="btn"/></button>
+  );
+} 
+export default Button;
