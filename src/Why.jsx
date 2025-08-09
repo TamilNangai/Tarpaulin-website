1bd5b7601211464eb9f87d6742d226593a24c528
Atchayasri1022 <atchayasribalasundaram@gmail.com>
2025-08-09 15:08:39 +0530

    Add files via upload

---
+++ src/Why.jsx
@@ -0,0 +1,26 @@
+const WhyChooseUs = () => {
+  return (
+  
+      <div className=" leading-loose mx-auto text-left text-xs md:max-w-5xl lg:max-w-5xl p-4 ">
+        <h2 className="text-5xl font-bold text-center  mb-6">Why Choose Us?</h2>
+
+        <ul className="text-2xl list-disc pl-5  ">
+          <li>
+            Quality Materials: We use only the best materials to ensure our tarps are tough, durable.
+          </li>
+          <li>
+              Customization: Need a specific size or feature? We offer custom tarpaulin solutions tailored to your requirement.
+          </li>
+          <li>
+              Expertise:With years of experience in the industry, our team has the knowledge to help you find the perfect tarpaulin for any application.
+          </li>
+          <li>
+              Customer Satisfaction:We are committed to providing exceptional customer service and ensuring our clients are completely satisfied with their purchases.
+          </li>
+        </ul>
+      </div>
+ 
+  );
+}
+
+export default WhyChooseUs;
