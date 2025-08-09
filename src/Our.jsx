3f7f59af6db0caf920e4aa83d568407027eea575
Atchayasri1022 <atchayasribalasundaram@gmail.com>
2025-08-09 15:07:36 +0530

    Add files via upload

---
+++ src/Our.jsx
@@ -0,0 +1,11 @@
+const OurMission =() => {
+return(
+   
+          <div class='px-2 text-black sm:max-w-3xl  lg:max-w-3xl md:max-w-3xl'>
+            <h2 class="text-3xl text-center ">Our Mission</h2>
+            <p >At Taurpauli, our mission is to provide reliable, durable, and versatile tarpaulin solutions to our customers. We strive to deliver excellence in every product, ensuring that each tarpaulin meets the highest standards of quality and performance.</p>
+          </div>
+  
+);
+}
+    export default OurMission;
